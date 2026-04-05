32c584fce12406ae21e5e622c55657ff5f06439d
Ana Júlia <anajuliasamorim19@gmail.com>
2026-04-05 14:40:14 -0400

    Corrigindo logica e estilo da funcao calcular_media

--- media_notas.py
+++ media_notas.py
@@ -1,6 +1,6 @@
 def calcular_media(notas):
-    soma=0
+    soma = 0
     for nota in notas:
         soma += nota
-    media = soma / (len(notas) - 1)
+    media = soma / len(notas)
     return media
